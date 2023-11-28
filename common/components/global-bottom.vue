f43bb4ddaf961597cdde778e18943526490f63ae
gangan <gangan.education@gmail.com>
2023-11-28 21:08:12 +0900

    add(slide1): add global-bottom.vue

---
+++ common/components/global-bottom.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="absolute bottom-0 left-0 right-0 pl-2 z-10 text-white">
+    <slot name="left">[ロゴ] 2023 @example corp.</slot>
+    <slot name="right"></slot>
+  </div>
+</template>
---
+++ slide1/global-bottom.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import CommonBottom from "../common/components/global-bottom.vue";
+</script>
+
+<template>
+  <CommonBottom />
+</template>
