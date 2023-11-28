5d87a8205f1ec3432c9e7e4b07642f7110fd10e0
gangan <gangan.education@gmail.com>
2023-11-29 01:04:37 +0900

    feat: add textlint

---
+++ .textlintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  rules: {
+    "no-mix-dearu-desumasu": true,
+    "no-double-negative-ja": true,
+    "no-hankaku-kana": true,
+    "no-dropping-the-ra": true,
+    // https://github.com/textlint-ja/textlint-rule-ja-no-mixed-period
+    "ja-no-mixed-period": true,
+    "prefer-tari-tari": true,
+    "@textlint-ja/no-synonyms": true,
+    "textlint-filter-rule-allowlist": true,
+    // https://github.com/textlint-ja/textlint-rule-preset-ja-spacing
+    "preset-ja-spacing": true,
+  },
+  filters: {},
+};
