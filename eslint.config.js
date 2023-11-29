6fd765808863bf8a23f1702b7cbebb4fe948616b
gangan <gangan.education@gmail.com>
2023-11-29 17:17:52 +0900

    feat: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+"use strict";
+// @ts-check
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfig from "@eslint/js";
+import { fileURLToPath } from "node:url";
+import { dirname } from "pathe";
+
+const eslintrc = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+});
+
+/** @type {import('eslint').Linter.FlatConfig} */
+export default [
+  {
+    ignores: [
+      "eslint.config.js",
+      "dist",
+      "**/node_modules/**/*",
+      ".github",
+      ".vscode",
+    ],
+  },
+  eslintConfig.configs.recommended,
+  ...eslintrc.extends("plugin:vue/vue3-recommended"),
+];
