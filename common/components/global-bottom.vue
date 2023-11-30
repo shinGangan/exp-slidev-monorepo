5b09e0940273053b3f74a2078a04fe01f47d5993
gangan <gangan.education@gmail.com>
2023-11-30 18:53:06 +0900

    fix(common): fix global-bottom component

--- common/components/global-bottom.vue
+++ common/components/global-bottom.vue
@@ -1,6 +1,16 @@
+<script setup lang="ts">
+const author = "example corp.";
+const href = "http://example.com";
+</script>
+
 <template>
   <div class="absolute bottom-0 left-0 right-0 pl-2 z-10 text-white">
-    <slot name="left">[ロゴ] 2023 @example corp.</slot>
+    <slot name="left">
+      [ロゴ] 2023 author:
+      <a :href="href">
+        {{ author }}
+      </a>
+    </slot>
     <slot name="right"></slot>
   </div>
 </template>
